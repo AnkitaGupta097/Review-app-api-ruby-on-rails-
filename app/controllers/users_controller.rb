60436228f80839f8a63e5ac73458aad3a20cd949
Ankita <ankita.gupta@zemosolabs.com>
2021-01-29 10:40:01 +0530

    small fixes in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,59 +1,53 @@
 class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users, include: [:posts]
+  end
 
-    def index
-     users= User.all
-     render json:users,include: [:posts]
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: { errors: user.errors }, status: 400
     end
+  end
 
-    def create
-        user=User.new(user_params)
-        if user.save
-            render json:user
-         else
-            render json:{errors:user.errors},status:400
-         end
-
+  def show
+    user = User.find_by(id: params[:id])
+    if user
+      render json: user, include: [:posts]
+    else
+      render json: { error: "User with id #{params[:id]} does not exist" }, status: 404
     end
+  end
 
-    def show
-      user=  User.find_by(id:params[:id])
-      if user
-      render json:user,include:[:posts]
+  def update
+    user = User.find_by(id: params[:id])
+    if user
+      if user.update(user_params)
+        render json: user
       else
-        render json:{error:"User with id #{params[:id]} does not exist"},status:404
+        render json: { errors: user.errors }, status: 400
       end
+    else
+      render json: { error: "User with id #{params[:id]} does not exist" }, status: 404
     end
+  end
 
-    def update
-        user=  User.find_by(id:params[:id])
-        if user
-           is_updated_user= user.update(user_params)
-            if is_updated_user  
-                 render json:user
-              else   
-                 render json:{errors:user.errors},status:400
-            end   
-        else
-          render json:{error:"User with id #{params[:id]} does not exist"},status:404
-        end
-
+  def destroy
+    user = User.find_by(id: params[:id])
+    if user.destroy
+      render json: user
+    else
+      render json: { error: 'delete operation not sucessfull' }, status: 400
     end
+  end
 
-    def destroy
-        user=  User.find_by(id:params[:id])
-      if user
-         destroyed_user=user.destroy
-          render json:destroyed_user
-      else
-        render json:{error:"User with id #{params[:id]} does not exist"},status:404
-       end
-    end 
+  private
 
-    private
-
-    def user_params
-       params.require(:user)  
-       params.permit(:name,:email)
-    end
-
+  def user_params
+    params.require(:user)
+    params.permit(:name, :email)
+  end
 end
